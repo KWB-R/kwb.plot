4468bdab6b2106e755e6878a85d5c259fe1dbea2
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-12-16 15:18:04 +0100

    Consider that extractSubstring() is exported

--- R/makeTransparent.R
+++ R/makeTransparent.R
@@ -4,7 +4,7 @@ makeTransparent <- function(colour_strings, alpha = 0.5)
   do.call(mapply, args = c(
     list(FUN = grDevices::rgb, MoreArgs = list(alpha = alpha)), 
     lapply(
-      kwb.utils:::extractSubstring(
+      kwb.utils::extractSubstring(
         pattern = "#(..)(..)(..)", 
         x = colour_strings, 
         index = c(red = 1, green = 2, blue = 3)
