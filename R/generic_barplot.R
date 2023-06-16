ad87cdc61d57ee182520f5f1f8b9486ea704cd09
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-06-16 05:09:26 +0200

    Add ... arguments to generic_barplot()
    and use \code{} and \link{} instead of
    backticks (markdown syntax)

--- R/generic_barplot.R
+++ R/generic_barplot.R
@@ -10,26 +10,28 @@
 #' Note: This description was written by the help of ChatGPT.
 #' 
 #' @param data A data frame containing the data for plotting.
-#' @param group_by The name of the column in `data` that contains the group 
-#' labels. Each unique value in this column corresponds to a distinct group, 
-#' and a bar will be generated for each group. The column must be of type 
-#' character or factor.
-#' @param values_in (Optional) The name of the column in `data` that contains
-#'   the values used to calculate the bar heights. If not provided, the bar
-#'   heights will be calculated based on the counts, i.e., the number of rows in
-#'   `data` that belong to each group as defined by the values in column
-#'   `group_by`.
-#' @param fill_by (Optional) The name of the column in `data` that defines
+#' @param group_by The name of the column in \code{data} that contains the group
+#'   labels. Each unique value in this column corresponds to a distinct group,
+#'   and a bar will be generated for each group. The column must be of type
+#'   character or factor.
+#' @param values_in (Optional) The name of the column in \code{data} that
+#'   contains the values used to calculate the bar heights. If not provided, the
+#'   bar heights will be calculated based on the counts, i.e., the number of
+#'   rows in \code{data} that belong to each group as defined by the values in
+#'   column \code{group_by}.
+#' @param fill_by (Optional) The name of the column in \code{data} that defines
 #'   sub-groups. Each sub-group will be represented by a different fill colour,
 #'   and the bars will consist of stacked, filled rectangles. This parameter is
 #'   useful when visualising sub-groups within each group.
-#' @param percentaged If set to `TRUE`, all bars will be stretched to extend to
-#'   the full height of the plot. This option is only applicable when `fill_by`
-#'   is specified where each bar is represented by a stack of rectangles each of
-#'   which is filled in a different colour. The heights of the rectangles will
-#'   then reflect the proportions of each sub-group (count or sum of
-#'   corresponding rows or values in the `values_in` column) within a group. The
-#'   default value is `FALSE`.
+#' @param percentaged If set to \code{TRUE}, all bars will be stretched to
+#'   extend to the full height of the plot. This option is only applicable when
+#'   \code{fill_by} is specified where each bar is represented by a stack of
+#'   rectangles each of which is filled in a different colour. The heights of
+#'   the rectangles will then reflect the proportions of each sub-group (count
+#'   or sum of corresponding rows or values in the \code{values_in} column)
+#'   within a group. The default value is \code{FALSE}.
+#' @param \dots further arguments passed to \code{\link[ggplot2]{geom_bar}}, 
+#'   such as \code{width} (bar width), \code{fill} (fill colour).
 #' @return A barplot visualising the data using ggplot.
 #' @importFrom ggplot2 aes geom_bar ggplot
 #' @importFrom rlang .data
@@ -97,6 +99,7 @@ generic_barplot <- function(
     ggplot2::geom_bar(
       mapping = fill_mapping,
       stat = ifelse(is.null(values_in), "count", "identity"),
-      position = ifelse(percentaged, "fill", "stack")
+      position = ifelse(percentaged, "fill", "stack"),
+      ...
     )
 }
